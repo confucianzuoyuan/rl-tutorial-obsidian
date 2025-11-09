f2c439875855770e18090cea4d9c2ec359996c2f
confucianzuoyuan <zuoyuantc@126.com>
2025-11-09 17:55:32 +0800

    update

--- llms-from-scratch/data.py
+++ llms-from-scratch/data.py
@@ -11,8 +11,8 @@ class DataSetV1(Dataset):
         token_ids = tokenizer.encode(txt)
 
         for i in range(0, len(token_ids) - max_length, stride):
-            input_chunk = token_ids[i : i + max_length]
-            target_chunk = token_ids[i + 1 : i + max_length + 1]
+            input_chunk = token_ids[i: i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
             self.input_ids.append(torch.tensor(input_chunk))
             self.target_ids.append(torch.tensor(target_chunk))
 
